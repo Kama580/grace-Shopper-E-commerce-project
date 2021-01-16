7990a149a0e2d5d4997bb01fbf489b5fe5155717
Kama580 <kleetal@gmail.com>
2021-01-16 12:52:12 -0500

    feat: remove item from cart

--- server/db/models/item-orders.js
+++ server/db/models/item-orders.js
@@ -1,6 +1,7 @@
 const Sequelize = require('sequelize')
 const db = require('../db')
 const Order = require('./orders')
+const Product = require('./products')
 
 const ItemOrder = db.define('itemOrder', {
   item_subtotal: {
@@ -15,6 +16,12 @@ const ItemOrder = db.define('itemOrder', {
       min: 1
     },
     defaultValue: 1
+  },
+  subtotal: {
+    type: Sequelize.VIRTUAL,
+    set() {
+      return Product.productId.price * this.qty
+    }
   }
 })
 
