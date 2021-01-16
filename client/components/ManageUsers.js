d55d6426708eaf926082d794fb095733f075f71f
May <mayyuhein@gmail.com>
2021-01-16 16:32:11 -0500

    Chore: Nav bar revamp

--- client/components/ManageUsers.js
+++ client/components/ManageUsers.js
@@ -1,16 +1,112 @@
 import React from 'react'
-import {Paper} from '@material-ui/core'
+// import {connect} from 'react-redux'
+// import {
+//   fetchUsers
+// } from '../store/allUsers'
+// import {
+//   List,
+//   Paper,
+//   ListItem,
+//   ListItemAvatar,
+//   ListItemText,
+//   Avatar,
+//   Fab,
+//   Button
+// } from '@material-ui/core'
 
-const ManageUsers = () => {
-  return (
-    <>
-      <div className="adminContainer">
-        <div className="adminListContainer">
-          <Paper>HELLO THIS IS USERS</Paper>
-        </div>
-      </div>
-    </>
-  )
+const defaultState = {
+  detailsFormOpen: false,
+  currentUserId: null
 }
 
+class ManageUsers extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      detailsFormOpen: false,
+      currentUserId: null
+    }
+  }
+
+  // async componentDidMount() {
+  //   try {
+  //     await this.props.getUsers()
+  //   } catch (error) {
+  //     console.log('This is componentDidMount error', error)
+  //   }
+  // }
+
+  // onOpenDetailsHandler(id) {
+  //   this.setState({
+  //     detailFormOpen: true,
+  //     currentProductId: id
+  //   })
+  // }
+
+  // onCloseDetailForm(event) {
+  //   this.setState(defaultState)
+  // }
+
+  render() {
+    return (
+      <div>HELLO ITS ME</div>
+      // <div className="adminContainer">
+      //   <div className="adminListContainer">
+
+      //     <Paper>
+      //       <List>
+      //         {this.props.users.map(user => {
+      //           return (
+      //               <div>
+      //               <ListItem key={user.id}>
+      //                 <ListItemAvatar>
+      //                   <Avatar src={user.imageUrl} />
+      //                 </ListItemAvatar>
+      //                 <ListItemText primary={user.firstName} />
+      //                 <Fab
+      //                   size="small"
+      //                   onClick={() => this.deleteHandler(product.id)}
+      //                 >
+      //                   <DeleteIcon />
+      //                 </Fab>
+      //                 <Fab
+      //                   size="small"
+      //                   onClick={() => this.onOpenHandler(product.id)}
+      //                 >
+      //                   <EditIcon />
+      //                 </Fab>
+      //               </ListItem>
+      //               {this.state.detailsFormOpen &&
+      //               this.state.currentUserId === user.id ? (
+      //                 <ListItem key={user.firstName}>
+      //                   <ListItem key={user.lastName}>
+      //                     <Paper>
+      //                     </Paper>
+      //                   </ListItem>
+      //                 </ListItem>)
+      //                       : null}
+      //             </div>
+      //           )
+      //         })}
+      //       </List>
+      //     </Paper>
+      //   </div>
+      // </div>
+    )
+  }
+}
+
+// const mapState = state => {
+//   return {
+//     users: state.users
+//   }
+// }
+
+// const mapDispatch = dispatch => {
+//   return {
+//     getUsers: () => dispatch(fetchUser())
+//   }
+// }
+
+// export default connect(mapState, mapDispatch)(ManageUsers)
 export default ManageUsers
