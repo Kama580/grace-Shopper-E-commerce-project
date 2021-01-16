132da5baf13b0fd94b94ac215626dec0cd8b06e9
Satomi Ichii <sichii@gmail.com>
2021-01-16 12:05:57 -0500

    chore: merge from main

--- server/api/cart.js
+++ server/api/cart.js
@@ -1,4 +1,5 @@
 const router = require('express').Router()
+const {ListItemAvatar} = require('@material-ui/core')
 const {User, Product, Order, ItemOrder} = require('../db/models')
 const {Pending} = require('../db/models/constant.js')
 
@@ -15,20 +16,33 @@ router.get('/:user', async (req, res, next) => {
   }
 })
 
-// router.put('/:user', async (req, res, next) => {
-//   try {
-//     const cart = await Order.findOne({
-//       where: {userId: req.params.user, status: Pending},
-//       include: {model: Product},
-//     })
-//     const items = cart.products
-//     const itemToUpdate = items.filter((item) => {
-//       return item.id === 2
-//     })
-//     res.send(cart)
-//   } catch (error) {
-//     next(error)
-//   }
-// })
+router.put('/:userId/:productId', async (req, res, next) => {
+  try {
+    const userId = req.params.userId
+    const productId = Number(req.params.productId)
+    const order = await Order.findOne({
+      where: {userId: userId, status: Pending},
+      include: {model: Product}
+    })
+    const items = await ItemOrder.findAll({
+      where: {productId: productId, orderId: order.id}
+    })
+    console.log(items)
+    // if (items.filter((item) => item.id === productId).length) {
+    //   items = items.map((item) => {
+    //     return
+    //     item.id === productId
+    //       ? {...item.itemOrder, qty: (item.itemOrder.qty += 1)}
+    //       : item
+    //   })
+
+    // const itemToUpdate = items.filter((item) => {
+    //   return item.id === 2
+    // })
+    res.send(res)
+  } catch (error) {
+    next(error)
+  }
+})
 
 module.exports = router
